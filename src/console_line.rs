cdb9807f8d86945e36d85ef4de824914ccc7db3c
TheNevix <ducelliersaimen@gmail.com>
2024-05-20 23:12:57 +0200

    Added cliclr and updated the code to make it work with this package.

--- src/console_line.rs
+++ src/console_line.rs
@@ -1,32 +1,21 @@
 use termcolor::{ColorSpec, StandardStream, WriteColor, Color};
-
-
-pub struct ConsoleLine {
-    pub text: String,
-    pub color: termcolor::Color
-}
-
-//fn to print colored text
-pub fn print_colored_text(line: ConsoleLine, stdout: &mut StandardStream){
-    stdout.set_color(ColorSpec::new().set_fg(Some(line.color))).unwrap();
-    println!("{}", line.text);
-    stdout.reset().unwrap();
-}
+use cliclr::print_colored_text;
+use cliclr::ConsoleLine;
 
 //text to print at the start
 pub fn print_start_screen(stdout: &mut StandardStream){
-    print_colored_text(ConsoleLine { text: String::from("Welcome to Nevix's version of hangman!"), color: Color::Red }, stdout);
+    print_colored_text(&ConsoleLine { text: String::from("Welcome to Nevix's version of hangman!"), color: Color::Red }, stdout);
     println!("");
-    print_colored_text(ConsoleLine { text: String::from("This awesome hangman game was made with the Rust programming language!"), color: Color::Red }, stdout);
-    print_colored_text(ConsoleLine { text: String::from("To play the game, type 'play', to get more info about the game, type 'info'"), color: Color::Red }, stdout);
+    print_colored_text(&ConsoleLine { text: String::from("This awesome hangman game was made with the Rust programming language!"), color: Color::Red }, stdout);
+    print_colored_text(&ConsoleLine { text: String::from("To play the game, type 'play', to get more info about the game, type 'info'"), color: Color::Red }, stdout);
 }
 
 pub fn print_info_screen(stdout: &mut StandardStream){
     println!("");
-    print_colored_text(ConsoleLine { text: String::from("Hangman is a simple word guessing game where you need guess the word before the hangman has been completed."), color: Color::Red }, stdout);
-    print_colored_text(ConsoleLine { text: String::from("You have a certain amount of guesses, all words are English words!"), color: Color::Red }, stdout);
+    print_colored_text(&ConsoleLine { text: String::from("Hangman is a simple word guessing game where you need guess the word before the hangman has been completed."), color: Color::Red }, stdout);
+    print_colored_text(&ConsoleLine { text: String::from("You have a certain amount of guesses, all words are English words!"), color: Color::Red }, stdout);
     println!("");
-    print_colored_text(ConsoleLine { text: String::from("Type 'play' to start."), color: Color::Red }, stdout);
+    print_colored_text(&ConsoleLine { text: String::from("Type 'play' to start."), color: Color::Red }, stdout);
 
 }
 
@@ -44,22 +33,22 @@ pub fn print_stripes_by_length(guessed_chars: &Vec<char>, word_to_guess: &Vec<ch
         }
     }
 
-    print_colored_text(ConsoleLine { text: stripes_to_print, color: Color::Red }, stdout)
+    print_colored_text(&ConsoleLine { text: stripes_to_print, color: Color::Red }, stdout)
 }
 
 pub fn print_ask_for_guess(guess_count: &u32, stdout: &mut StandardStream){
     let text = format!("Enter guess {}", guess_count);
-    print_colored_text(ConsoleLine { text: text, color: Color::Red }, stdout);
+    print_colored_text(&ConsoleLine { text: text, color: Color::Red }, stdout);
 }
 
 pub fn print_guess_found(guess: &char, stdout: &mut StandardStream){
     let text = format!("You guessed a correct letter! The word contains at least 1 '{}'", guess);
-    print_colored_text(ConsoleLine { text: text, color: Color::Green }, stdout);
+    print_colored_text(&ConsoleLine { text: text, color: Color::Green }, stdout);
 }
 
 pub fn print_guess_not_found(guess: &char, stdout: &mut StandardStream){
     let text = format!("You guessed a incorrect letter! The word does not contain the letter '{}'", guess);
-    print_colored_text(ConsoleLine { text: text, color: Color::Red}, stdout);
+    print_colored_text(&ConsoleLine { text: text, color: Color::Red}, stdout);
 }
 
 pub fn print_hangman_phase(phase: &u32, stdout: &mut StandardStream){
@@ -159,19 +148,19 @@ pub fn print_hangman_phase(phase: &u32, stdout: &mut StandardStream){
 
 pub fn print_lost_text(word_to_guess: &String, stdout: &mut StandardStream){
     let text = format!("You ran out of guesses and lost! The word was '{}'", word_to_guess);
-    print_colored_text(ConsoleLine { text: text, color: Color::Red }, stdout);
+    print_colored_text(&ConsoleLine { text: text, color: Color::Red }, stdout);
 }
 
 pub fn print_win_text(word_to_guess: &String, amount_of_guesses: &u32, stdout: &mut StandardStream){
     let text = format!("You win! The word is '{}'. You got the anwser in {} guesses!", word_to_guess, amount_of_guesses);
-    print_colored_text(ConsoleLine { text: text, color: Color::Green }, stdout);
+    print_colored_text(&ConsoleLine { text: text, color: Color::Green }, stdout);
 }
 
 pub fn print_ask_another_game(stdout: &mut StandardStream){
-    print_colored_text(ConsoleLine { text: String::from("Would you like to play another game of hangman?"), color: Color::Red }, stdout);
-    print_colored_text(ConsoleLine { text: String::from("Type 'yes' to play another game of hangman and 'no' to quit."), color: Color::Red }, stdout);
+    print_colored_text(&ConsoleLine { text: String::from("Would you like to play another game of hangman?"), color: Color::Red }, stdout);
+    print_colored_text(&ConsoleLine { text: String::from("Type 'yes' to play another game of hangman and 'no' to quit."), color: Color::Red }, stdout);
 }
 
 pub fn print_alr_guessed_letter(stdout: &mut StandardStream){
-    print_colored_text(ConsoleLine { text: String::from("You already guessed that letter! Try to guess a different letter!"), color: Color::Red }, stdout);
+    print_colored_text(&ConsoleLine { text: String::from("You already guessed that letter! Try to guess a different letter!"), color: Color::Red }, stdout);
 }
